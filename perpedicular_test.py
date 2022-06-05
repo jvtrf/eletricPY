deaf0f7701d0c358ae20e4be70cdcdb1744e4a3c
VictorCalebeIFG <victorcalebeacd@gmail.com>
2022-06-05 03:10:06 -0300

    Linhas Perpendiculares

--- perpedicular_test.py
+++ perpedicular_test.py
@@ -9,15 +9,38 @@ canvas = tk.Canvas(root,width=1300,height=700, background='white')
 canvas.pack()
 
 
-A = 0,0
-B = 400,500
+A = 100,500 # Funciona
+B = 400,100 #
+
+X = 100,100
+Y = 500,500
 
 canvas.create_line(A,B)
 
-inter,head = perpendicular_point(A,B,d=40)
+points,head = draw_curve(A,B,d = 40,dir=1)
 
 create_circle(canvas,head,5)
 
-canvas.create_line(inter,head)
+canvas.create_line(points)
+
+#canvas.create_line(A,B)
+
+#points,head = draw_curve(A,B,d = 40,dir=0)
+
+#canvas.create_line(points)
+
+#create_circle(canvas,head,5)
+
+#create_circle(canvas,point,5)
+
+#canvas.create_line((0,0),(A))
+
+
+
+#inter,head = perpendicular_point(B,A,d=40)
+
+#create_circle(canvas,head,5)
+
+#canvas.create_line(inter,head)
 
 root.mainloop()
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
-from math import sin, radians,cos,atan2,degrees
+from math import sin, radians,cos,atan2,degrees,acos
+from numpy import linspace
 
 def angle_sin(ang):
     #Calcula o seno de um angulo
@@ -33,19 +34,23 @@ def percent_line(A,B,p):
     return point
 
 def create_circle(canvas,c,r):
-    #Cria uma cricunferencia vermelha, com centro "c" e rairo "r".
-    #Utilizado pra debugar.
+    # Cria uma cricunferencia vermelha, com centro "c" e rairo "r".
+    # Utilizado pra debugar.
 
     p1 = c[0]-r,c[1]-r
     p2 = c[0]+r,c[1]+r
     canvas.create_oval(p1,p2,fill='red')
 
 def get_mid_p(A,B):
-    #Obtem o midpoint da reta.
+    # Obtem o midpoint da reta.
     return (A[0]+B[0])/2 , (A[1]+B[1])/2
 
 def perpendicular_point(A,B,d,p = 0.5, dir = 0):
-    
+    # Cria um vetor perpendicular ao vetor ab.
+    # O parâmetro "p" deve estrar entre 0 e 1 (float), é ele que decide quão posição começa o vetor perpendicular.
+    # O parâmetro dir, decide qual lado em relação ao vetor AB a reta perpendicular vai estar.
+    # Retorna (inter_point, head)
+
     vec = get_vec_A_B(A,B)
 
     inter_point = percent_line(A,B,p)
@@ -54,17 +59,60 @@ def perpendicular_point(A,B,d,p = 0.5, dir = 0):
 
     if (ang_vec >=0 and ang_vec<= 90) or (ang_vec>=-180 and ang_vec<=-90):
         if ang_vec>=-180 and ang_vec<=-90: ang_vec +=180
-        #Cabeça do vetor na direção pra baixo a direita (quarto quadrante).
-        per_ang = -(90 - ang_vec)  +dir*(180)#angulo perpendicular
+        #Cabeça do vetor no quarto quadrante ou no segundo.
+        per_ang = -(90 - ang_vec)  +dir*(180) #Angulo perpendicular
         head = (inter_point[0] + d*angle_cos(per_ang), inter_point[1] + d*angle_sin(per_ang)) #Cabeça do vetor
     else:
-        per_ang = (90 - ang_vec)  + dir*(180)
+        #Cabeça do vetor no primeiro ou no terceiro quadrante.
+        if ang_vec <= -180 and ang_vec >= -270: ang_vec += 180
+        per_ang = -(90 - ang_vec)  + dir*(180)
         head = (inter_point[0] + d*angle_cos(per_ang), inter_point[1] + d*angle_sin(per_ang)) #Cabeça do vetor
     
-    print(ang_vec)
     return (inter_point,head)
     pass
 
+def draw_curve(A,B,d,dir = 0):
+    inter_point,head = perpendicular_point(A,B,d,p = 0.5,dir = dir)
+    
+    vec_1 = get_vec_A_B(head,A)
+    vec_2 = get_vec_A_B(head,B)
+
+    line = get_vec_A_B(A,B)
+
+    raio = get_vec_module(vec_1) #Tamanho do raio 
+
+    
+    ang1 = angle_vector(vec_1)
+    ang2 = angle_vector(vec_2) + dir*360
+
+    angles = linspace(ang1,ang2,20).tolist()
+    angles = [round(a,2) for a in angles]
+
+    print(ang1,ang2)
+    points =  [(head[0]+raio*angle_cos(a),head[1]+raio*angle_sin(a)) for a in angles]
+    
+    points = [(round(p[0],2),round(p[1],2)) for p in points]
+
+    
+
+    return points,head
+
 def get_vec_A_B(A,B):
+    #Retorna o vetor AB (indo de A para B).
     return (B[0] - A[0], B[1] - A[1])
     pass
+
+def get_vec_module(vec):
+    # Retorna o módulo de um vetor
+    return (vec[0]**2 + vec[1]**2)**(0.5)
+
+def produto_escalar(A,B):
+    # Retorna o produto escalar entre AB.
+    return A[0]*B[0] + A[1]*B[1]
+
+def get_angle_between(vec1,vec2):
+    #Retorna o ângulo entre dois vetores.
+    cos_ang = (produto_escalar(vec1,vec2))/(get_vec_module(vec1)*get_vec_module(vec2))
+    return acos(cos_ang)
+    pass
+
